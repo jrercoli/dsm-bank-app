3c08b1248fe3857193efbe076f101bbb671c4f72
jrercoli <rorjor@gmail.com>
2021-07-21 18:59:13 -0300

    add request service exceptions

--- application/routes.py
+++ application/routes.py
@@ -31,15 +31,28 @@ def bankclassify():
 
     # url for bank marketing model
     # Test
-    url = "http://localhost:5000/api"
+    url = "http://localhost:3000/api"
     # Stage
     # url = "https://bank-model-app.herokuapp.com/api"
   
     # post data to url
-    results =  requests.post(url, input_data)
+    conn_exception = ''
+    results = ''
+    try:
+        result = requests.post(url, input_data)
+        # decode to UTF-8
+        results = result.content.decode('UTF-8')
+
+    except requests.exceptions.Timeout:
+        conn_exception = 'Timeout, retry later'
+        pass
+
+    except requests.exceptions.RequestException as e:
+        conn_exception = 'Prediction service not available'
+        pass
 
     # send input values and prediction result to index.html for display
-    return render_template("index.html", bank_image=bank_image,
+    return render_template("index.html", bank_image=bank_image, conn_exception=conn_exception,
                            age=age, job=job, marital=marital, education=education,
                            default=default, balance=balance, housing=housing,
-                           loan=loan,  results=results.content.decode('UTF-8'))
+                           loan=loan,  results=results)
